9e18ca9592c5f328a43b1f7ec38ac098e3a36de5
Semen Zhuravlev <zhuravlev.more@gmail.com>
2023-12-16 23:33:01 +0100

    added commentService crud

--- src/main/java/zhurasem/project/business/CommentService.java
+++ src/main/java/zhurasem/project/business/CommentService.java
@@ -2,14 +2,44 @@ package zhurasem.project.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import zhurasem.project.api.exceptions.EntityStateException;
 import zhurasem.project.dao.CommentJpaRepository;
 import zhurasem.project.domain.Comment;
+import zhurasem.project.domain.Petition;
+import zhurasem.project.domain.User;
+
+import java.util.Optional;
 
 @Service
 public class CommentService extends AbstractCrudService<Comment, Long>{
 
+    private final UserService userService;
+    private final PetitionService petitionService;
+
     @Autowired
-    public CommentService(CommentJpaRepository commentJpaRepository) {
+    public CommentService(CommentJpaRepository commentJpaRepository, UserService userService, PetitionService petitionService) {
         super(commentJpaRepository);
+        this.userService = userService;
+        this.petitionService = petitionService;
+    }
+
+    @Override
+    public Comment create(Comment entity) throws EntityStateException {
+        Optional<User> optUser = userService.readById(entity.getAuthorComment().getUsername());
+        Optional<Petition> optPetition = petitionService.readById(entity.getPetitionComment().getPid());
+        if(optUser.isEmpty() || optPetition.isEmpty())
+            throw new EntityStateException();
+        return super.create(entity);
+    }
+
+    @Override
+    public Comment update(Comment entity) throws EntityStateException {
+        Optional<User> optUser = userService.readById(entity.getAuthorComment().getUsername());
+        Optional<Petition> optPetition = petitionService.readById(entity.getPetitionComment().getPid());
+        if(optUser.isEmpty() || optPetition.isEmpty())
+            throw new EntityStateException();
+        if(entity.getText().isEmpty())
+            throw new EntityStateException();
+        return super.update(entity);
     }
 }
